7e29976974eb8abf99e86d39aa0785e7b6d2a1e3
kate <chimmi10kate@mail.ru>
2022-05-26 00:04:00 +0300

    redesigned first root

--- client/components/main.jsx
+++ client/components/main.jsx
@@ -9,21 +9,21 @@ const Main = (props) => {
     const onClick = () => {
       props.changeState(str)
     }
-    return <div className="w-screen h-screen flex items-center justify-center bg-gradient-to-tr from-emerald-900 to-lime-600">
-           <div className="bg-white w-1/4 h-1/4 flex items-center place-content-center">
-          <input
+    return <div className="w-screen h-screen flex items-center justify-center bg-gradient-to-tr from-green-500 to-teal-800">
+           <div className="bg-gradient-to-br from-green-500 to-teal-800  w-1/4 h-1/4 flex items-center place-content-center rounded-md border-2 border-emerald-400 border-double ">      
+            <input
            type="text"
            name="username"
            id="username"
-           className="block w-50% pl-7 pr-12 sm:text-sm rounded-full border border-green-600 placeholder-white antialiased bg-gradient-to-tl from-emerald-800 to-lime-400 ring-inherit outline-inherit text-emerald-200 font-mono font-semibold"
+           className="block w-50% pl-7 pr-12 sm:text-sm rounded-full border border-green-300 placeholder-white antialiased bg-gradient-to-tl from-green-500 to-teal-800 outline-inherit text-emerald-200 font-mono font-semibold"
            placeholder="Username"
            autoComplete="given-name"
            value={str}
            onChange={onChange}
-          />
+            />
           <button
            type="button"
-           className="inline-block px-6 py-2.5 mt-3 border-2 border-orange-500 bg-green-500 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-green-600 hover:shadow-lg focus:bg-emerald-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-500 active:shadow-lg transition duration-150 ease-in-out font-mono antialiased"
+           className="inline-block px-6 py-2.5 mt-1 ml-2 bg-teal-900 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-teal-600 hover:shadow-lg focus:bg-emerald-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-teal-500 active:shadow-lg transition duration-150 ease-in-out font-mono antialiased"
            onClick={onClick}
           >
             Search
